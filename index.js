20b1ba837cf52776611ff0699bd37307e787d0ce
klhe0002 <klhe0002@stud.kea.dk>
2025-02-03 15:31:03 +0100

    done

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const listContainer = document.querySelector(".category_list_container");
 
 fetch(`https://kea-alt-del.dk/t7/api/categories`) /* laver en fetch(asynkron metode) og linker til API(json data)*/
   .then((response) => response.json())
-  .then(showCategory);
+  .then((data) => showCategory(data));
 
-function showCategory(data) {
-  const markup = data
+function showCategory(categories) {
+  const markup = categories
     .map(
       //løbe mit array igennem
       (element) => `
